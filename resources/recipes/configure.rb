5dd5659618b7313e5d0d16b7c40a0c1016bb7c9e
cjmateos <mateos83@gmail.com>
2017-01-31 10:58:50 +0100

    Modify standard configuration recipe

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -7,6 +7,13 @@
 # AFFERO GENERAL PUBLIC LICENSE V3
 #
 
+# Load keys from s3_secrets data bag
+#s3_databag = Chef::DataBagItem.load("passwords", "s3") rescue s3_secrets = {}
+
+nginx_config "config" do
+  action :add
+end
+
 riak_config_riak "Riak config" do
   config_dir node["riak"]["config_dir"]
   logdir node["riak"]["logdir"]
@@ -19,41 +26,20 @@ end
 riak_config_riakcs "Riak-cs config" do
   config_dir node["riak-cs"]["config_dir"]
   logdir node["riak-cs"]["logdir"]
-  riakcs_ip node["riak-cs"]["ip"]
-  riakcs_port node["riak-cs"]["port"]
   riak_ip node["riak"]["ip"]
   riak_port node["riak"]["port"]
-  stanchion_ip node["stanchion"]["ip"]
-  stanchion_port node["stanchion"]["port"]
-  cdomain node["redborder"]["cdomain"]
-  action :config
-end
-
-riak_config_stanchion "stanchion config" do
-  config_dir node["stanchion"]["config_dir"]
-  logdir node["stanchion"]["logdir"]
-  stanchion_ip node["stanchion"]["ip"]
-  stanchion_port node["stanchion"]["port"]
   riakcs_ip node["riak-cs"]["ip"]
   riakcs_port node["riak-cs"]["port"]
-  riak_ip node["riak"]["ip"]
-  riak_port node["riak"]["port"]
+  stanchion_ip node["stanchion"]["ip"]
+  stanchion_port node["stanchion"]["port"]
+  #s3_access GET FROM DATA BAG
+  #s3_secret GET FROM DATA BAG
   action :config
-end
-
-riak_config_riakcs "Riak-cs create user" do
-  s3cfg_file node["riak-cs"]["s3cfg_file"]
-  action :create_user
 end
 
 riak_config_riakcs "Riak-cs set proxy" do
   proxy_conf node["riak-cs"]["proxy_conf"]
   riakcs_ip node["riak-cs"]["ip"]
   riakcs_port node["riak-cs"]["port"]
-  cdomain node["redborder"]["cdomain"]
   action :set_proxy
-end
-
-riak_config_riakcs "Riak-cs create buckets" do
-  action :create_buckets
 end
