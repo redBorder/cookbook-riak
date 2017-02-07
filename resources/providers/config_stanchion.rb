7cfcf2a0b901be07238a16403847c6be96fc49a5
cjmateos <mateos83@gmail.com>
2017-02-07 18:58:26 +0100

    WIP

--- resources/providers/config_stanchion.rb
+++ resources/providers/config_stanchion.rb
@@ -2,6 +2,60 @@
 # Provider:: config_stanchion
 #
 
+action :config_default do
+  begin
+
+    config_dir = new_resource.config_dir
+    logdir = new_resource.logdir
+    user = new_resource.user
+    group = new_resource.group
+
+    riak_ip = new_resource.riak_ip
+    riak_port = new_resource.riak_port
+
+    riakcs_ip = new_resource.riakcs_ip
+    riakcs_port = new_resource.riakcs_port
+
+    stanchion_ip = new_resource.stanchion_ip
+    stanchion_port = new_resource.stanchion_port
+
+    s3_access = "admin-key"
+    s3_secret = "admin-secret"
+
+    yum_package "stanchion" do
+      action :upgrade
+      flush_cache [ :before ]
+    end
+
+    user user do
+      group group
+      action :create
+    end
+
+    template "#{config_dir}/stanchion.conf" do
+      source "stanchion.conf.erb"
+      owner user
+      group group
+      mode 0644
+      retries 2
+      notifies :restart, "service[stanchion]", :delayed
+      variables(:stanchion_ip => stanchion_ip, :stanchion_port => stanchion_port, \
+        :riakcs_ip => riakcs_ip, :riakcs_port => riakcs_port, :riak_ip => riak_ip, :riak_port => riak_port, \
+        :s3_access => s3_access, :s3_secret => s3_secret, :logdir => logdir)
+    end
+
+    service "stanchion" do
+      service_name "stanchion"
+      supports :status => true, :reload => true, :restart => true, :start => true, :enable => true
+      action [:enable,:start]
+    end
+
+    Chef::Log.info("Riak-cs cookbook has been processed")
+  rescue => e
+    Chef::Log.error(e.message)
+  end
+end
+
 action :config do
   begin
 
