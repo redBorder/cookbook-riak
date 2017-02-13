71009cb0f302f656d1de13b95aa502f51d616630
cjmateos <mateos83@gmail.com>
2017-02-13 18:11:45 +0100

    WIP

--- resources/recipes/configure_solo.rb
+++ resources/recipes/configure_solo.rb
@@ -11,6 +11,13 @@
 init_conf = YAML.load_file("/etc/redborder/rb_init_conf.yml")
 cdomain = init_conf['cdomain']
 
+# Get stored S3 information
+begin
+  s3_user = Chef::JSONCompat.parse(::File.read('/etc/redborder/s3user.json'))
+rescue
+  s3_user = {"key_id" => "admin-key", "key_secret" => "admin-secret"}
+end
+
 nginx_config "config" do
   action :add_solo
 end
@@ -34,8 +41,10 @@ riak_config_riakcs "Riak-cs config" do
   riakcs_port node["riak-cs"]["port"]
   stanchion_ip node["stanchion"]["ip"]
   stanchion_port node["stanchion"]["port"]
+  s3_access s3_user['key_id']
+  s3_secret s3_user['key_secret']
   cdomain cdomain
-  action :config_default
+  action :config
 end
 
 # First time is called s3_access & s3_secret has default values
@@ -48,52 +57,11 @@ riak_config_stanchion "stanchion config" do
   riakcs_port node["riak-cs"]["port"]
   stanchion_ip node["stanchion"]["ip"]
   stanchion_port node["stanchion"]["port"]
-  action :config_default
+  s3_access s3_user['key_id']
+  s3_secret s3_user['key_secret']
+  action :config
 end
 
 riak_config_riakcs "Riak-cs create user" do
   action :create_user
-end
-
-# Get stored S3 information
-ruby_block 'get_s3_keys' do
-  block do
-    begin
-      s3_user = Chef::JSONCompat.parse(::File.read('/etc/redborder/s3user.json'))
-    rescue
-      s3_user = {"key_id" => "admin-key", "key_secret" => "admin-secret"}
-    end
-  end
-  action :run
-end
-
-# Second time is called s3_access & s3_secret has the new user values
-riak_config_riakcs "Riak-cs config" do
-  config_dir node["riak-cs"]["config_dir"]
-  logdir node["riak-cs"]["logdir"]
-  riak_ip node["riak"]["ip"]
-  riak_port node["riak"]["port"]
-  riakcs_ip node["riak-cs"]["ip"]
-  riakcs_port node["riak-cs"]["port"]
-  stanchion_ip node["stanchion"]["ip"]
-  stanchion_port node["stanchion"]["port"]
-  s3_access s3_user['key_id']
-  s3_secret s3_user['key_secret']
-  cdomain cdomain
-  action :config
-end
-
-# Second time is called s3_access & s3_secret has the new user values
-riak_config_stanchion "stanchion config" do
-  config_dir node["stanchion"]["config_dir"]
-  logdir node["stanchion"]["logdir"]
-  riak_ip node["riak"]["ip"]
-  riak_port node["riak"]["port"]
-  riakcs_ip node["riak-cs"]["ip"]
-  riakcs_port node["riak-cs"]["port"]
-  stanchion_ip node["stanchion"]["ip"]
-  stanchion_port node["stanchion"]["port"]
-  s3_access s3_user['key_id']
-  s3_secret s3_user['key_secret']
-  action :config
 end
